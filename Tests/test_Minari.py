5eeaa6dac13ef9677995546c100c1ba04b40e09d
Winsor Tse <wtse8@gatech.edu>
2026-08-21 21:13:00 -0400

    testing Minari

--- Tests/test_Minari.py
+++ Tests/test_Minari.py
@@ -2,11 +2,14 @@ import minari
 
 dataset = minari.load_dataset("D4RL/door/human-v2")
 
-for episode_data in dataset.iterate_episodes():
-    observations = episode_data.observations
-    actions = episode_data.actions
-    rewards = episode_data.rewards
-    terminations = episode_data.terminations
-    truncations = episode_data.truncations
-    infos = episode_data.infos
-    print(observations)
+print("Observation space:", dataset.observation_space)
+print("Action space:", dataset.action_space)
+print("Total episodes:", dataset.total_episodes)
+print("Total steps:", dataset.total_steps)
+
+for i in range(5):
+    # sample 5 episodes from the dataset
+    episodes = dataset.sample_episodes(n_episodes=5)
+    # get id's from the sampled episodes
+    ids = list(map(lambda ep: ep.id, episodes))
+    print(f"EPISODE ID'S SAMPLE {i}: {ids}")
